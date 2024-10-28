2f2affb70a7faa09714d834c6a30affd1b534c80
Evan <evanaguttesrivelon@gmail.com>
2024-10-28 14:39:42 +0100

    debut du js, creation de la constante QUIZ qui contient un tableau d'objet avec la question, les differentes reponses et la bonne reponse

---
+++ questions.js
@@ -0,0 +1,26 @@
+export const QUIZ = {
+	questions: [
+		{
+			text: "Qui à dit : La vie est un mystère qu'il faut vivre, et non un problème à résoudre ?",
+			options: [
+				"John F. Kennedy",
+				"Ghandi",
+				"Antoine De Saint Exupery",
+				"Le Buddha"
+			],
+			correct_answer: "Ghandi"
+		}/* ,
+		{
+
+		},
+		{
+
+		},
+		{
+
+		},
+		{
+
+		} */
+	]
+}
