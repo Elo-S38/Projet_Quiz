465057787915207342c1febfd57bcf99caab1457
Evan <evanaguttesrivelon@gmail.com>
2024-10-28 16:36:24 +0100

    completion du premier theme de questions

--- questions.js
+++ questions.js
@@ -1,7 +1,7 @@
 export const QUIZ = {
 	questions: [
 		{
-			text: "Qui à dit : La vie est un mystère qu'il faut vivre, et non un problème à résoudre ?",
+			text: "Qui à dit : La vie est un mystère qu'il faut vivre, et non un problème à résoudre.",
 			options: [
 				"John F. Kennedy",
 				"Ghandi",
@@ -9,18 +9,36 @@ export const QUIZ = {
 				"Le Buddha"
 			],
 			correct_answer: "Ghandi"
-		}/* ,
-		{
-
 		},
 		{
-
+			text: "Qui à dit : Seuls ceux qui sont assez fous pour penser qu'ils peuvent changer le monde y parviennent.",
+			options: [
+				"Einstein",
+				"Ghandi",
+				"Churchill",
+				"Steve Jobs"
+			],
+			correct_answer: "Steve Jobs"
 		},
 		{
-
+			text: "Il ne faut pas attendre d'être parfait pour commencer quelque chose de bien.",
+			options: [
+				"Les Frères Lumière",
+				"L'Abbé Pierre",
+				"Karl Lagerfeld",
+				"Keanu Reeves"
+			],
+			correct_answer: "L'Abbé Pierre"
 		},
 		{
-
-		} */
+			text: "Je préfère risquer en osant, que regretter de ne pas avoir su saisir l’occasion qui se présentait.",
+			options: [
+				"Marilyn Monroe",
+				"Donald Trump",
+				"Nicolas Sarkozy",
+				"Coco Chanel"
+			],
+			correct_answer: "Nicolas Sarkozy"
+		}
 	]
 }
